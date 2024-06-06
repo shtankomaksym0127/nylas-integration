a2cea10ac37c86ddaef2a19c3dcdbcf071eff086
maksym <shtankomaksym1997@outlook.com>
2024-06-06 14:48:24 +0300

    feat: fetch events data

--- app/auth/service.ts
+++ app/auth/service.ts
@@ -1,18 +1,14 @@
-import { getToken } from '@/connectors/auth';
+import { getToken } from "@/connectors/auth";
+import { getCalendarEvents } from "@/connectors/event";
 
 type SetupOrganisationParams = {
   code: string;
 };
 
-export const setupOrganisation = async ({
-  code,
-}: SetupOrganisationParams) => {
-  
+export const setupOrganisation = async ({ code }: SetupOrganisationParams) => {
   // retrieve token from SaaS API using the given code
   const { accessToken, grantId } = await getToken(code);
 
-console.log("accessToken", accessToken)
-    
-//   const timeZone = await getAccountDetails(accessToken);
-
+  const events = await getCalendarEvents(accessToken, grantId);
+  console.log("events:", events);
 };
